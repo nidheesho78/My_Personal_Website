94f24af9e06126bf0bd3f64686075dd3871926f8
nidheesho78 <127939143+nidheesho78@users.noreply.github.com>
2023-03-24 11:49:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".multiple-text", {
+  strings:["Full Stack Java Developer","Full Stack Developer",
+  "Java Developer","App Developer"],
+  typeSpeed:100,
+  backSpeed:100,
+  backDelay:1000,
+  loop:true
+
+})
